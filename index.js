6414f3297985ee14752f0c26d2ac437ad170abaf
Ｍ♢ＮＴΛＳＩＭ <montasimmamun@gmail.com>
2023-07-14 02:15:15 +0600

    [ADDED] user api

--- index.js
+++ index.js
@@ -113,6 +113,26 @@ const run = async () => {
         res.status(404).json({ error: 'book not found' });
       }
     });
+
+    app.post('/user', async (req, res) => {
+      const user = req.body;
+
+      const result = await userCollection.insertOne(user);
+
+      res.send(result);
+    });
+
+    app.get('/user/:email', async (req, res) => {
+      const email = req.params.email;
+
+      const result = await userCollection.findOne({ email });
+
+      if (result?.email) {
+        return res.send({ status: true, data: result });
+      }
+
+      res.send({ status: false });
+    });
   } finally {
   }
 };
